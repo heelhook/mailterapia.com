dbacedd0a94214298c929e4e957608717d8192a5
Pablo Fernandez <heelhook@littleq.net>
2015-12-22 11:32:07 +0100

    Add vacation display support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :load_coupon
+  before_filter :load_vacations
 
   # before_action :authenticate_user!
 
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
     else
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
     end
+  end
+
+  def load_vacations
+    @vacations_until = DateTime.parse('2016-01-07').to_time
   end
 
   def load_coupon
